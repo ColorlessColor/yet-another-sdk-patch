debeac90eaee3cb153b36af8d6607c35ceb517a0
Misaka-L <lipww1234@foxmail.com>
2025-12-03 13:57:27 +0800

    feat: description and category property for patch

--- src/Packages/xya.misakal.vpm.yes-patch-framework-for-sdk/PatchApi/YesPatchBase.cs
+++ src/Packages/xya.misakal.vpm.yes-patch-framework-for-sdk/PatchApi/YesPatchBase.cs
@@ -4,6 +4,9 @@ public abstract class YesPatchBase
 {
     public abstract string Id { get; }
     public abstract string DisplayName { get; }
+    public virtual string Category { get; } = "Uncategorized";
+    public virtual string Description { get; } = "No description provided.";
+
     public virtual bool IsDefaultEnabled { get; } = false;
 
     public virtual void Configure(IYesPatchConfigureOptions configureOptions)
